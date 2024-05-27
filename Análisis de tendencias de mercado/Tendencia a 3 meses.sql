85383d91472acf70ee361c1aeeb4e40bd47f36c5
federicochecozzi <tiamantex@hotmail.com>
2024-05-26 22:57:01 -0300

    Progresando con el script de tendencia a 3 meses

--- Análisis de tendencias de mercado/Tendencia a 3 meses.sql
+++ Análisis de tendencias de mercado/Tendencia a 3 meses.sql
@@ -1,6 +1,7 @@
-WITH OrderQtyPerMonth([Year],[Month],Subcat,OrderQty) AS(
-	SELECT dd.CalendarYear, dd.MonthNumberOfYear, dp.ProductSubcategoryKey,
-	SUM(fis.OrderQuantity) OrderQty
+WITH OrderQtyPerMonth([Year],[Month],MonthNumber,Subcat,OrderQty) AS(
+	SELECT DISTINCT dd.CalendarYear, dd.MonthNumberOfYear, dp.ProductSubcategoryKey,
+	ROW_NUMBER() OVER(PARTITION BY dp.ProductSubcategoryKey ORDER BY dd.CalendarYear, dd.MonthNumberOfYear) AS MonthNumber,
+	SUM(fis.OrderQuantity) OVER(PARTITION BY dp.ProductSubcategoryKey ORDER BY dd.CalendarYear, dd.MonthNumberOfYear) AS OrderQty
 	FROM AdventureWorksDW2019.dbo.FactInternetSales fis, 
 		AdventureWorksDW2019.dbo.DimProduct dp, 
 		AdventureWorksDW2019.dbo.DimProductSubcategory dps,
@@ -8,10 +9,9 @@ WITH OrderQtyPerMonth([Year],[Month],Subcat,OrderQty) AS(
 	WHERE fis.ProductKey = dp.ProductKey AND 
 		dp.ProductSubcategoryKey = dps.ProductSubcategoryKey AND 
 		fis.OrderDateKey = dd.DateKey 
-	GROUP BY dd.CalendarYear, dd.MonthNumberOfYear, dp.ProductSubcategoryKey
+	--GROUP BY dd.CalendarYear, dd.MonthNumberOfYear, dp.ProductSubcategoryKey
 )
-SELECT Subcat,[Year],[Month],
-	
+SELECT *	
 FROM OrderQtyPerMonth
 ORDER BY Subcat,[Year],[Month]
 
